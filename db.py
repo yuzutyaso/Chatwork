37d55dde3a06bc8756744decee0f6e3ebec1a76a
yuzutyaso <f.kokori85@gmail.com>
2025-08-22 08:36:31 +0900

    Update db.py

--- db.py
+++ db.py
@@ -1,11 +1,10 @@
 import os
-from dotenv import load_dotenv
 from supabase import create_client, Client
+from dotenv import load_dotenv
 
 load_dotenv()
 
-SUPABASE_URL: str = os.getenv("SUPABASE_URL")
-SUPABASE_KEY: str = os.getenv("SUPABASE_KEY")
-
-# Supabaseクライアントをグローバルに定義
+# Supabaseクライアントの初期化
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
