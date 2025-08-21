47e3c94878d7c46b47cba9bb8ca3797475714858
yuzutyaso <f.kokori85@gmail.com>
2025-08-21 15:47:35 +0900

    Delete utils.py

--- utils.py
+++
@@ -1,6 +0,0 @@
-import re
-
-def clean_message_body(body):
-    """Removes Chatwork-specific tags from the message body."""
-    body = re.sub(r'\[rp aid=\d+ to=\d+-\d+\]|\[piconname:\d+\].*?さん|\[To:\d+\]', '', body)
-    return body.strip()
